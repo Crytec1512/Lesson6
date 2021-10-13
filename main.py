b80a8939ef49a3ce9085a002791df78d17954279
Дмитрий Малахов <339616@itip.loc>
2021-10-13 18:49:01 +0300

    First try

---
+++ main.py
@@ -0,0 +1,33 @@
+class Person:
+    arms_count = 2 #статическая переменная
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+
+        return f"Person<name={self.name}>"
+
+    def __eq__(self, other):
+
+        return self.name == other.name
+
+    def greet(self):
+        print(f'Hi {self.name}!')
+
+me = Person("Nick")
+clone = Person("Nick")
+you = Person("Vasya")
+
+print(me.arms_count, you.arms_count)
+
+Person.arms_count = 5
+
+print(me.arms_count, you.arms_count)
+
+print(me,you)
+
+print(me == clone)
+
+me.greet()
+you.greet()
